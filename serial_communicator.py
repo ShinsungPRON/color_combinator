5d94f029968f595c6e5e1a4a23d1f12558e1431b
flyahn06 <flyahn06@gmail.com>
2023-10-25 08:27:28 +0900

    완성

--- serial_communicator.py
+++ serial_communicator.py
@@ -7,6 +7,9 @@ import socket
 config = configparser.ConfigParser()
 config.read("./conf.conf")
 
+with open("color_table.json", "r") as f:
+    colors = json.load(f)
+
 ser = serial.Serial(
     port=config['DEFAULT']['serialPort'],
     baudrate=9600
@@ -19,17 +22,31 @@ soc.listen(1)
 server, addr = soc.accept()
 
 while True:
-    data = server.recv(256)
+    data = json.loads(server.recv(256))
 
-    ser.write(data['code'])
+    R, Y, B, P = colors[data['code']]
+
+    ser.write("begin")
+    ser.write(R)
+    ser.write(Y)
+    ser.write(B)
+    ser.write(P)
 
     while not ser.readable():
         time.sleep(0.1)
 
     suc = ser.read()
 
-    soc.send(json.dumps({
-        "_id": data["_id"],
-        "status": "done",
-        "device": config['DEFAULT']['deviceName']
-    }).encode())
+    if suc == 'done':
+        soc.send(json.dumps({
+            "_id": data["_id"],
+            "status": "done",
+            "device": config['DEFAULT']['deviceName']
+        }).encode())
+
+    else:
+        soc.send(json.dumps({
+            "_id": data["_id"],
+            "status": "error",
+            "device": config['DEFAULT']['deviceName']
+        }).encode())
